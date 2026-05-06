beef8d4077428fb3d20ed7ace4ff2e4d969004e3
Elizabeth Parks <elizabethparks@arizona.edu>
2026-05-06 02:36:25 +0000

    database connection problems

---
+++ sql/northcarolina/01_osm_hospital_distribution.sql
@@ -0,0 +1,17 @@
+-- Query 1: Hospital Locations
+-- Purpose: Extract hospital locations for spatial distribution analysis
+
+-- Requirements:
+-- - Use pois for point features
+-- - Filter POIs where fclass = 'hospitalrestaurant'
+-- - Return raw point geometries
+
+-- Expected Output:
+-- - geom
+
+SELECT
+    geom
+FROM
+    pois
+WHERE
+    fclass = 'hospital';
